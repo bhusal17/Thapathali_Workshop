9642d25c13360ff06b7b29f9c8968b3750802c26
bhusal17 <bhusal.17@gmail.com>
2018-06-13 23:49:37 +0545

    functions to access port added

--- gpio_interface.c
+++ gpio_interface.c
@@ -22,7 +22,23 @@ void write_gpio_pin(GPIORegister_structure *GPIOX, uint8_t pin_number, uint8_t o
 	GPIOX->OutputDatareg = ( GPIOX->OutputDatareg & ( ~ ( 1<<pin_number ) ) ) | ( output_value << pin_number );
 }
 
+//Function for writing to GPIOx_Output_Register
+
+void write_gpio_port(GPIORegister_structure *GPIOX, uint8_t, uint8_t output_value){
+
+        GPIOX->OutputDatareg = ( GPIOX->OutputDatareg  | output_value );
+}
+
+//Function for reading GPIOx_Input_Register
+
+uint16_t read_gpio_port(GPIORegister_structure *GPIOX){
+ 
+        return( GPIOX->InputDataReg );
+}
+
 void enable_gpio_clock(uint8_t GPIO_PORT_Number){
 
     RCC->APB2ENR |= ( 1 << ( GPIO_PORT_Number + 2) );
 }
+
+
