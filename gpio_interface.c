b250ad04cbb9f9b12562092d3aa9225121e91e7b
Arjun Bhusal <bhusal.17@gmail.com>
2018-06-13 17:21:43 +0545

    functions to access gpio port

---
+++ gpio_interface.c
@@ -0,0 +1,28 @@
+#include "port_struct.h"
+
+void configure_gpio_pin(GPIORegister_structure *GPIOX, uint8_t pin_number, uint8_t pin_mode){
+
+	if(pin_number < 8){
+		GPIOX->ConfigRegLow = ( GPIOX->ConfigRegLow & ( ~ ( 1<< pin_number ) ) ) | (pin_mode << pin_number );
+
+	}
+	else{
+		pin_number = pin_number - 8;
+		GPIOX->ConfigRegLow = ( GPIOX->ConfigRegLow & ( ~ ( 1<< pin_number ) ) ) | (pin_mode << pin_number );
+	}
+}
+
+uint8_t read_gpio_pin(GPIORegister_structure *GPIOX, uint8_t pin_number){
+
+	return( ( GPIOX->InputDataReg >> pin_number ) & 0x01 );
+}
+
+void write_gpio_pin(GPIORegister_structure *GPIOX, uint8_t pin_number, uint8_t output_value){
+
+	GPIOX->OutputDatareg = ( GPIOX->OutputDatareg & ( ~ ( 1<<pin_number ) ) ) | ( output_value << pin_number );
+}
+
+void enable_gpio_clock(uint8_t GPIO_PORT_Number){
+
+    RCC->APB2ENR |= ( 1 << ( GPIO_PORT_Number + 2) );
+}
