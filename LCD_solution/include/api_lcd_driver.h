01439feb2539d56195023520dd4ec51491ff7d0f
bhusal17 <bhusal.17@gmail.com>
2018-06-15 11:31:56 +0545

    LCD solutions

---
+++ LCD_solution/include/api_lcd_driver.h
@@ -0,0 +1,156 @@
+#ifndef _LCD_Library_H_
+#define  _LCD_Library_H_
+
+
+//following macros are defined here, letter define these on the application file
+
+#define LCD_CONTROL_PORT	GPIO_PORT_A
+#define LCD_DATA_PORT		GPIO_PORT_D
+#define LCD_EnablePin		PB2
+#define LCD_RWPin			PB1
+#define LCD_ModeSelectPin	PB0
+
+
+
+#define F_CPU 16000000
+#include <util/delay.h>
+#include "hal_gpio_driver.h"
+
+//gpio_port_conf_data *gpio_lcd_config_data;
+
+
+
+//Macros definition
+//Define these macros in your program at the top
+
+#define LCD_WRITE_MODE		0x00
+#define LCD_READ_MODE		0x01
+#define LCD_COMMAND_MODE	0x00
+#define LCD_DATA_MODE		0x01
+#define LCD_LATCH_ENABLE	0x01
+
+/*
+#ifndef LCD_COMMUNICATION_MODE
+# warning "LCD Communication mode not defined, the default mode is 8-bit"
+#define LCD_COMMUNICATION_MODE LCD_MODE_A
+#endif
+//future work needed to set the data communication mode
+
+#ifndef LCD_DATA_PORT
+# warning "LCD_DATA_PORT Not defined, the program will not work"
+#define LCD_DATA_PORT GPIO_PORT_B
+#endif
+  
+#ifndef	LCD_CONTROL_PORT
+# warning "LCD_CONTROL_PORT not defined, the program will not work"
+#define LCD_CONTROL_PORT GPIO_PORT_D
+#endif
+
+#ifndef LCD_RWPin
+# warning "LCD_RWPin pin not defined"
+#define LCD_RWPin 1
+#endif
+
+#ifndef LCD_ModeSelectPin
+# warning "LCD_ModeSelectPin not defined"
+#define LCD_ModeSelectPin 2
+#endif
+
+#ifndef LCD_EnablePin
+# warning "LCD_EnablePin not defined"
+#define LCD_EnablePin 3
+#endif
+*/
+
+
+
+//Functions Declarations
+
+/**********************************************************************************************************************************************
+	
+							User exposed API for accssing the Alphanumeric LCD library
+	
+***********************************************************************************************************************************************/	
+
+	/**			LCD initialization functions
+	*	@brief	This function initialize LCD after power up
+				It sets the lcd for the AXB size matrix
+				It also sets the data communication mode, i.e 4bit or 8bit mode 
+	*	@param	None
+	*	@retval	None
+	*	@Constraints: After power up, wait atleast 15ms before initializing the LCD
+	*/     
+void apiLCD_initializeLCD();
+
+
+/**					Clear the LCD screen
+	*	@brief	This function clears the LCD and puts cursor to (0,0) 
+	*	@param	None
+	*	@retval None
+	*	@Constraints: Execution time (when FCP/FOSC is 250kHz) for clear display
+	*				  command is 1.64ms. The time decreases at higher frequency
+	*				  Look LCD datasheets for reference.
+	*/
+void apiLCD_clear_screen();
+
+	
+/**					Makes the display off
+	*	@brief	This function makes the display screen off, 
+				but the data sent to it for display is not lost 
+	*	@param	None
+	*	@retval None
+	*/
+void apiLCD_display_screenOFF();
+
+
+/**					Makes the display screen on 
+	*	@brief	This function makes the display screen on, 
+				previously stored data is displayed on screen. 
+	*	@param	None
+	*	@retval None
+	*/
+void apiLCD_display_screenON();
+
+	
+	
+/**
+	*	@brief	This module is used to send the character to display on 
+				LCD screen at current cursor position
+	*	@param	LCDcharacter:	User desired character to be displayed on 
+				the LCD screen at current cursor position
+	*	@retval	None
+	*/
+	
+void apiLCD_send_character(char LCDcharacter);
+
+
+/**
+	*	@brief	This function is used to send the string to display on the LCD screen
+	*	@param	LCDstring:	This is the string that is to be displayed on the LCD screen
+							at current cursor position
+	*	@retval	None
+	*/
+void apiLCD_send_string(char *LCDstring);
+
+
+/**
+	*	@brief	This function is used to send the numerical values to the LCD
+				Numbers can be directly sent to the LCD as a string using send string function
+				But to send the numeric data from sensors we use this function.
+	*	@param	LCDnumber:	The numeric value to be displayed on the screen.
+	*	@retval	None
+	*/
+void apiLCD_send_number(int LCDnumber);
+
+/**
+	*	@brief	This function is used to position the cursor at desired position on LCD screen
+	*	@param	x:	The cursor position along the row from the left side
+					For 20X4 LCD the x can have value from 0 to 20.
+	*	@param	y:	The cursor position along the column from the top
+					For 20X4 LCD the y can have value from 0 to 4
+	*	@retval	None
+	*/
+	
+void apiLCD_set_coursertoXY(unsigned int x, unsigned int y);
+
+#endif
---
+++ LCD_solution/src/api_lcd_driver.c
@@ -0,0 +1,220 @@
+#include "api_lcd_driver.h"
+
+gpio_port_conf_data gpio_lcd_config_data;
+
+
+/**					Send command to LCD
+	*	@brief	This function sends user command to LCD 
+	*	@param	LCDcommand: The user desired command to LCD
+	*	@retval None
+	*	@Constraints: Execution time (when FCP/FOSC is 250kHz) for clear display
+	*				  and return home command is 1.64ms. The time decreases at
+	*				   higher frequency. Look LCD datasheets for other commands.
+	*/
+	
+void localLCD_check_status(){
+
+	gpio_lcd_config_data.port_mode_type = GPIO_PORT_INPUT_MODE;
+	gpio_lcd_config_data.port_input_type = GPIO_PORT_INPUT_TYPE_TRISTATE;
+	hal_gpio_port_setup(LCD_DATA_PORT, &gpio_lcd_config_data); 
+	
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_RWPin, LCD_READ_MODE);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_ModeSelectPin, LCD_COMMAND_MODE);
+	//uint8_t pinvalue = 1;
+	//while( pinvalue == 1)
+	//{
+		//set lcd latch enable pin to high
+		hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_HIGH);
+		_delay_us(100);
+		hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_LOW);
+		//pinvalue = hal_gpio_read_from_pin(LCD_DATA_PORT, PB7 );
+	//}
+	//set the LCD data port to output mode 
+	
+	gpio_lcd_config_data.port_mode_type = GPIO_PORT_OUTPUT_MODE;
+	hal_gpio_port_setup(LCD_DATA_PORT, &gpio_lcd_config_data);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_RWPin, LCD_WRITE_MODE);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_ModeSelectPin, LCD_DATA_MODE);
+}
+
+/**					Send command to LCD
+	*	@brief	This function sends user command to LCD 
+	*	@param	LCDcommand: The user desired command to LCD
+	*	@retval None
+	*	@Constraints: Execution time (when FCP/FOSC is 250kHz) for clear display
+	*				  and return home command is 1.64ms. The time decreases at
+	*				   higher frequency. Look LCD datasheets for other commands.
+	*/
+void localLCD_send_command (uint8_t LCDcommand){
+	
+	localLCD_check_status();//at the end of this function lcd is in data write mode and data port in write mode
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_ModeSelectPin, LCD_COMMAND_MODE);
+	hal_gpio_write_to_port(LCD_DATA_PORT, LCDcommand);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_HIGH);
+	_delay_us(1);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_LOW);
+	
+}
+
+//setups pins of the microcontroller to which lcd is connected
+void localLCD_pins_setup(){
+		
+		gpio_pin_conf_t lcd_pin_setup_data;
+		lcd_pin_setup_data.mode_type = GPIO_PIN_OUTPUT_MODE;
+		lcd_pin_setup_data.pin = LCD_EnablePin;
+		hal_gpio_init(LCD_CONTROL_PORT, &lcd_pin_setup_data);
+		
+		lcd_pin_setup_data.mode_type = GPIO_PIN_OUTPUT_MODE;
+		lcd_pin_setup_data.pin = LCD_RWPin;
+		hal_gpio_init(LCD_CONTROL_PORT, &lcd_pin_setup_data);
+		
+		lcd_pin_setup_data.mode_type = GPIO_PIN_OUTPUT_MODE;
+		lcd_pin_setup_data.pin = LCD_ModeSelectPin;
+		hal_gpio_init(LCD_CONTROL_PORT, &lcd_pin_setup_data);
+		
+		gpio_lcd_config_data.port_mode_type = GPIO_PORT_OUTPUT_MODE;
+		hal_gpio_port_setup(LCD_DATA_PORT, &gpio_lcd_config_data);
+				
+		
+}
+		
+//Functions Definitions
+
+/**********************************************************************************************************************************************
+	
+							User exposed API for accssing the Alphanumeric LCD library
+	
+***********************************************************************************************************************************************/	
+
+	/**			LCD initialization functions
+	*	@brief	This function initialize LCD after power up
+				It sets the lcd for the AXB size matrix
+				It also sets the data communication mode, i.e 4bit or 8bit mode 
+	*	@param	None
+	*	@retval	None
+	*	@Constraints: After power up, wait atleast 15ms before initializing the LCD
+	*/     
+void apiLCD_initializeLCD(){
+	//future work:: write this function to accept the data for data mode, data length, cursor on/off
+	
+	
+	localLCD_pins_setup();
+	
+//hal_gpio_write_to_port(LCD_DATA_PORT, LCDcommand);
+	
+	
+	_delay_ms(15);
+	localLCD_send_command(0x38);	//This is the first command to lcd, it sets the data mode to 8 bit wit 2 display lines adnd 5*8 fonts
+	_delay_us(80);
+	localLCD_send_command(0x0C);	// This command make the display on and cursor on, cursor blinking on
+	_delay_us(80);
+	localLCD_send_command(0x01);	//this command clears the display and set cursor to 0,0
+	_delay_ms(2);
+//	localLCD_send_command(0x02);
+//	_delay_ms(2);
+	
+}
+
+
+/**					Clear the LCD screen
+	*	@brief	This function clears the LCD and puts cursor to (0,0) 
+	*	@param	None
+	*	@retval None
+	*	@Constraints: Execution time (when FCP/FOSC is 250kHz) for clear display
+	*				  command is 1.64ms. The time decreases at higher frequency
+	*				  Look LCD datasheets for reference.
+	*/
+void apiLCD_clear_screen(){
+	
+	localLCD_send_command(0x01);
+	_delay_ms(2);
+}
+
+
+/**					Makes the display off
+	*	@brief	This function makes the display screen off, 
+				but the data sent to it for display is not lost 
+	*	@param	None
+	*	@retval None
+	*/
+
+void apiLCD_display_screenOFF(){
+	
+	localLCD_send_command(0x08);
+}
+
+
+/**					Makes the display screen on 
+	*	@brief	This function makes the display screen on, 
+				previously stored data is displayed on screen. 
+	*	@param	None
+	*	@retval None
+	*/
+void apiLCD_display_screenON(){
+	
+	localLCD_send_command(0x0E);
+}
+	
+
+/**
+	*	@brief	This module is used to send the character to display on 
+				LCD screen at current cursor position
+	*	@param	LCDcharacter:	User desired character to be displayed on 
+				the LCD screen at current cursor position
+	*	@retval	None
+	*/
+	
+void apiLCD_send_character(char LCDcharacter){
+
+	localLCD_check_status();
+	hal_gpio_write_to_port(LCD_DATA_PORT,LCDcharacter);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_HIGH);
+	_delay_us(1);
+	hal_gpio_write_to_pin(LCD_CONTROL_PORT, LCD_EnablePin, GPIO_PIN_OUTPUT_LOW);
+}
+
+
+/**
+	*	@brief	This function is used to send the string to display on the LCD screen
+	*	@param	LCDstring:	This is the string that is to be displayed on the LCD screen
+							at current cursor position
+	*	@retval	None
+	*/
+void apiLCD_send_string(char *LCDstring){
+	
+	while(*LCDstring != 0)
+	{
+		apiLCD_send_character(*LCDstring);
+		LCDstring += 1;
+	}	
+}
+
+
+/**
+	*	@brief	This function is used to send the numerical values to the LCD
+				Numbers can be directly sent to the LCD as a string using send string function
+				But to send the numeric data from sensors we use this function.
+	*	@param	LCDnumber:	The numeric value to be displayed on the screen.
+	*	@retval	None
+	*/
+void apiLCD_send_number(int LCDnumber){
+	
+	char LCDstring[5];
+	itoa(LCDnumber,LCDstring,10);
+	apiLCD_send_string(LCDstring);
+}
+
+/**
+	*	@brief	This function is used to position the cursor at desired position on LCD screen
+	*	@param	x:	The cursor position along the row from the left side
+					For 20X4 LCD the x can have value from 0 to 20.
+	*	@param	y:	The cursor position along the column from the top
+					For 20X4 LCD the y can have value from 0 to 4
+	*	@retval	None
+	*/
+	
+void apiLCD_set_coursertoXY(unsigned int x, unsigned int y){
+
+	unsigned int LCD_firstColumn[] = {0x80,0xC0, 0x94, 0xD4};
+	localLCD_send_command( LCD_firstColumn[y] + x );	
+}
